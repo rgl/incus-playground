df4b287d578eaf7099c6fa87efc0251e054fc2a1
Rui Lopes <rgl@ruilopes.com>
2024-01-27 10:07:55 +0000

    upgrade to mrparkers/keycloak 4.4.0

--- keycloak/providers.tf
+++ keycloak/providers.tf
@@ -6,7 +6,7 @@ terraform {
     # see https://registry.terraform.io/providers/mrparkers/keycloak
     keycloak = {
       source  = "mrparkers/keycloak"
-      version = "4.3.1"
+      version = "4.4.0"
     }
   }
   backend "local" {
