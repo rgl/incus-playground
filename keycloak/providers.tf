1c86a4fa199e2a94ae448f2ee6d3c7faf5645aee
Rui Lopes <rgl@ruilopes.com>
2024-02-06 07:13:06 +0000

    upgrade to terraform 1.7.2

--- keycloak/providers.tf
+++ keycloak/providers.tf
@@ -1,6 +1,6 @@
 # see https://github.com/hashicorp/terraform
 terraform {
-  required_version = "1.7.1"
+  required_version = "1.7.2"
   required_providers {
     # see https://github.com/mrparkers/terraform-provider-keycloak
     # see https://registry.terraform.io/providers/mrparkers/keycloak
