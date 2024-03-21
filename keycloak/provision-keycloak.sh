8a27ae33a657ca8a8c9e8b5fc68db44e764f4f9a
Rui Lopes <rgl@ruilopes.com>
2024-03-21 21:26:43 +0000

    upgrade to keycloak 24.0.1

--- keycloak/provision-keycloak.sh
+++ keycloak/provision-keycloak.sh
@@ -6,7 +6,7 @@ base_url="https://$domain:8443"
 
 # see https://github.com/keycloak/keycloak/releases
 # renovate: datasource=github-releases depName=keycloak/keycloak
-keycloak_version='23.0.7'
+keycloak_version='24.0.1'
 
 # install dependencies.
 apt-get install -y unzip openjdk-17-jre-headless
