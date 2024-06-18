4a2a0fb1b0befe72004fbce513202368761a443d
Rui Lopes <rgl@ruilopes.com>
2024-06-18 19:29:47 +0100

    upgrade to incus 6.2.0

--- provision-incus.sh
+++ provision-incus.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 openfga_domain="${1:-pandora.incus.test}"; shift || true
 domain="${1:-incus.test}"; shift || true
-incus_version="${1:-6.0}"; shift || true
+incus_version="${1:-6.2}"; shift || true
 storage_driver="${1:-btrfs}"; shift || true
 storage_device='/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_incus'
 
@@ -166,11 +166,16 @@ EOF
 incus profile show default --project default
 
 # create the example projects (configured like the default profile).
-incus project create foo
+# see ProjectPut at https://github.com/lxc/incus/blob/v6.2.0/shared/api/project.go#L30-L43
+incus project create foo <<'EOF'
+description: Example Foo
+EOF
 incus profile show default --project default \
   | incus profile edit default --project foo
 incus profile show default --project foo
-incus project create bar
+incus project create bar <<'EOF'
+description: Example Bar
+EOF
 incus profile show default --project default \
   | incus profile edit default --project bar
 incus profile show default --project bar
