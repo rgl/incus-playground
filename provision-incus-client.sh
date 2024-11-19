29c5d4b1db04e8df1aeb1304933bcae1bc21254b
Rui Lopes <rgl@ruilopes.com>
2024-11-19 07:19:13 +0000

    upgrade to incus 6.7.0

--- provision-incus-client.sh
+++ provision-incus-client.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 # see https://github.com/lxc/incus/releases
-incus_client_version="${1:-6.6.0}"
+incus_client_version="${1:-6.7.0}"
 
 # download and install.
 incus_url="https://github.com/lxc/incus/releases/download/v${incus_client_version}/bin.linux.incus.x86_64"
