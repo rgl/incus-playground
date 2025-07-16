a1a841b71841ce706587ee5151be98452e8e6bf0
Rui Lopes <rgl@ruilopes.com>
2025-07-16 06:22:35 +0000

    upgrade to openfga cli 0.7.2

--- provision-openfga-cli.sh
+++ provision-openfga-cli.sh
@@ -5,7 +5,7 @@ OPENFGA_FQDN="${1}"
 
 # see https://github.com/openfga/cli/releases
 # renovate: datasource=github-releases depName=openfga/cli
-openfga_cli_version='0.6.6'
+openfga_cli_version='0.7.2'
 
 # download and install the fga cli.
 # see https://github.com/openfga/cli/releases
