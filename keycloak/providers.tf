973da0326f030c2a5807f40742530077b94c2a6a
Rui Lopes <rgl@ruilopes.com>
2025-07-16 06:22:35 +0000

    upgrade the keycloak/keycloak terraform provider to 5.3.0

--- keycloak/providers.tf
+++ keycloak/providers.tf
@@ -6,7 +6,7 @@ terraform {
     # see https://registry.terraform.io/providers/keycloak/keycloak
     keycloak = {
       source  = "keycloak/keycloak"
-      version = "5.2.0"
+      version = "5.3.0"
     }
   }
   backend "local" {
