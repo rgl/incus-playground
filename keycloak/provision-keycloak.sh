7a4a93c9535d1cec56a0a63d5e2d186619b59a25
Rui Lopes <rgl@ruilopes.com>
2025-09-18 07:30:11 +0000

    upgrade to keycloak 26.3.4

--- keycloak/provision-keycloak.sh
+++ keycloak/provision-keycloak.sh
@@ -6,7 +6,7 @@ base_url="https://$domain:8443"
 
 # see https://github.com/keycloak/keycloak/releases
 # renovate: datasource=github-releases depName=keycloak/keycloak
-keycloak_version='26.3.2'
+keycloak_version='26.3.4'
 
 # install dependencies.
 apt-get install -y unzip openjdk-21-jre-headless
