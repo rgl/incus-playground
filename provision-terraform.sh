0354b996c083c77ca458e1690b057ffa82130074
Rui Lopes <rgl@ruilopes.com>
2025-09-18 07:30:11 +0000

    upgrade to terraform 1.13.2

--- provision-terraform.sh
+++ provision-terraform.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # see https://github.com/hashicorp/terraform/releases
 # renovate: datasource=github-releases depName=hashicorp/terraform
-terraform_version='1.13.0'
+terraform_version='1.13.2'
 
 # install terraform.
 wget -q https://releases.hashicorp.com/terraform/$terraform_version/terraform_${terraform_version}_linux_amd64.zip
