dec6f686d765f3e78b08e0f481b277eef7d124f4
Rui Lopes <rgl@ruilopes.com>
2025-10-13 22:59:59 +0000

    upgrade to openfga 1.10.2

--- provision-openfga.sh
+++ provision-openfga.sh
@@ -6,7 +6,7 @@ OPENFGA_FQDN="${1}"
 
 # see https://github.com/openfga/openfga/releases
 # renovate: datasource=github-releases depName=openfga/openfga
-openfga_version='1.10.0'
+openfga_version='1.10.2'
 
 # create the openfga system user.
 groupadd --system openfga
@@ -43,8 +43,8 @@ popd
 
 # configure.
 # see https://openfga.dev/docs/getting-started/setup-openfga/configure-openfga
-# see Config at https://github.com/openfga/openfga/blob/v1.10.0/pkg/server/config/config.go#L302
-# see DefaultConfig at https://github.com/openfga/openfga/blob/v1.10.0/pkg/server/config/config.go#L686
+# see Config at https://github.com/openfga/openfga/blob/v1.10.2/pkg/server/config/config.go#L300
+# see DefaultConfig at https://github.com/openfga/openfga/blob/v1.10.2/pkg/server/config/config.go#L684
 cat >/opt/openfga/config.yaml <<EOF
 log:
   format: text
