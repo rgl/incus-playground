b9e6c9881a2b59aa286616024f78263e00351bf6
Rui Lopes <rgl@ruilopes.com>
2025-11-29 08:41:14 +0000

    upgrade to terraform 1.14.0

--- keycloak/providers.tf
+++ keycloak/providers.tf
@@ -1,6 +1,6 @@
 # see https://github.com/hashicorp/terraform
 terraform {
-  required_version = "1.13.4"
+  required_version = "1.14.0"
   required_providers {
     # see https://github.com/keycloak/terraform-provider-keycloak
     # see https://registry.terraform.io/providers/keycloak/keycloak
